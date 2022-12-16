439b1aec354cc882bc0f742dc920fe331a4f5d04
svillegasinacap <samuel.villegas03@inacapmail.cl>
2022-12-16 02:11:47 -0300

    me equivoque

--- colegio/app/models.py
+++ colegio/app/models.py
@@ -8,3 +8,10 @@ class Alumno(models.Model):
     curso = models.CharField(max_length=30,default='')
     userName = models.CharField(max_length=30,default='')
     password = models.CharField(max_length=50,default='')
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=20,default='')
+    apellido = models.CharField(max_length=20,default='')
+    titulo = models.CharField(max_length=30,default='')
+    userName = models.CharField(max_length=30,default='')
+    password = models.CharField(max_length=50,default='')
--- colegio/app/views.py
+++ colegio/app/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render 
+from django.shortcuts import render
 from app.models import *
+from .forms import ProfesorForm
+
 
 def panelAlumno(request,id):
     alumno = Alumno.objects.get(id=id)
 
     datos = {'alumno':alumno}
     return render(request,'panelAlumno.html',datos)
+
+
+def agregarProfesor(request):
+    data = {
+        'form': ProfesorForm
+    }
+    if request.method == "POST":
+        formulario = ProfesorForm(data=request.POST, files=request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+        else:
+            data['form'] = formulario
+    return render(request, 'profesor/agregar.html', data)
+
